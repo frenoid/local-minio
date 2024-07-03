66c732df4dd2fc2ca808290845a4005581348fac
Lim Xing Kang, Norman <frenoid@gmail.com>
2024-07-03 17:20:58 +0000

    Update the start-minio.sh script to use --restart always flag

--- start-minio.sh
+++ start-minio.sh
@@ -8,4 +8,5 @@ docker run \
    --env-file=./.env \
    -v /data/minio:/data \
    -v /data/certs/minio:/opts/certs \
+   --restart unless-stopped \
    quay.io/minio/minio:RELEASE.2024-06-06T09-36-42Z server /data --console-address ":9001" --certs-dir "/opts/certs"
